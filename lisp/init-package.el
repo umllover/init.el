c7dd77cf03e66eca4b5180478cf630c71e34e6ba
zhangdengqi <9682950@qq.com>
2017-08-23 11:25:09 +0800

    update

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,60 +1,64 @@
 
 ;; ----------Package Management
 ;; --- source ---------
- (when (>= emacs-major-version 24)
-     (require 'package)
-     (package-initialize)
-     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-              ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+(require 'package)
+(add-to-list 'package-archives
+	     '("gnu"   . "http://elpa.emacs-china.org/gnu/") t)
+(add-to-list 'package-archives
+	     '("melpa" . "http://elpa.emacs-china.org/melpa/") t)
  
-;; cl - Common Lisp Extension
-(require 'cl);; loop 为common lisp 的扩展
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
  
 ;; Add Packages
-(defvar my/packages '(
-           ;; --- Auto-completion ---
-	   auto-complete
-	   ;; -- ecb
-	   ecb
-           ;; --- Better Editor ---
-           evil
-           evil-leader
-           smooth-scrolling
-           hungry-delete
-           swiper
-           counsel
-           smartparens
-           ;; popwin
-           ;; -- python
-           elpy
-	   ;; -- webmode
-	   web-mode
-	   js2-mode
-           ;; --- Themes ---
-           monokai-theme
-           ;; --- Window Jump --
-           ace-link
-           window-numbering
-	   ;; -- c/c++
-	   xcscope
-	   ;; -- git ---
-	   magit
-           ) "Default packages")
+(defvar my-packages
+  '(   auto-complete
+       ;; -- ecb
+       ecb
+       ;; --- Better Editor ---
+       evil
+       evil-leader
+       smooth-scrolling
+       hungry-delete
+       swiper
+       counsel
+       smartparens
+       ;; -- python
+       elpy
+       ;; -- webmode
+       web-mode
+       js2-mode
+       ;; --- Themes ---
+       monokai-theme
+       ;; --- Window Jump --
+       ace-link
+       window-numbering
+       ;; -- c/c++
+       xcscope
+       ;; -- git ---
+       magit) "Default packages")
+
+(mapc #'(lambda (package)
+	   (unless (package-installed-p package)
+	     (package-install package)))
+       my-packages)
 
 
-(setq package-selected-packages my/packages)
- 
-(defun my/packages-installed-p ()
-    (loop for pkg in my/packages
-      when (not (package-installed-p pkg)) do (return nil)
-      finally (return t)))
- 
-(unless (my/packages-installed-p)
-    (message "%s" "Refreshing package database...")
-    (package-refresh-contents)
-    (dolist (pkg my/packages)
-      (when (not (package-installed-p pkg))
-    (package-install pkg))))
+;;
+;;(setq package-selected-packages my/packages)
+;; 
+;;(defun my/packages-installed-p ()
+;;    (loop for pkg in my/packages
+;;      when (not (package-installed-p pkg)) do (return nil)
+;;      finally (return t)))
+;; 
+;;(unless (my/packages-installed-p)
+;;    (message "%s" "Refreshing package database...")
+;;    (package-refresh-contents)
+;;    (dolist (pkg my/packages)
+;;      (when (not (package-installed-p pkg))
+;;    (package-install pkg))))
  
 (provide 'init-package)
  
